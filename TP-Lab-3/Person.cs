231618b8f9e7941a339629c88784179654c864f3
jumanandez <juanma08072003@gmail.com>
2024-04-03 17:57:29 -0300

    Cargadas Las clases: -Person -Student -College

---
+++ TP-Lab-3/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Lab_3
+{
+    public class Person
+    {
+        public string FirstName
+        { get; set; }
+
+        public string LastName 
+        { get; set; }
+
+        public DateTime BirthDate
+        { get; set; }
+
+        private int age
+        {get; set;}
+        public int Age
+        {
+            get
+            {
+                DateTime fechaActual = DateTime.Now;
+                this.age = fechaActual.Year - BirthDate.Year;
+
+                if (BirthDate > (fechaActual.AddYears(-this.age)))
+                {
+                    this.age--;
+                }
+
+                return this.age;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} {Age}";
+        }
+
+        public string FullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+
+
+    }
+}
