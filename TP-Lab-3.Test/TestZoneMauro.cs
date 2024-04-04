3695533d630313f29ef0834f60c93cdd5873c070
Mauro Zini <maurozinixd@gmail.com>
2024-04-04 14:38:15 -0300

    Cree la TestZoneMauro para los testeo

---
+++ TP-Lab-3.Test/TestZoneMauro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TP_Lab_3.Test
+{
+    public class TestZoneMauro
+    {
+        [Fact]
+        public class Ejemplo_de_test
+        {
+
+        }
+    }
+}
